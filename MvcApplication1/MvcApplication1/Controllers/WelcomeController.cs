c2f81dc759ba227a2ae5d465ff6d7581594ae2ef
Morgan <morgan.hoke@gmail.com>
2013-09-16 14:44:30 -0500

    Added ASP.NET MVC Demo Project

---
+++ MvcApplication1/MvcApplication1/Controllers/WelcomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class WelcomeController : Controller
+    {
+        //
+        // GET: /Welcome/
+
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+
+    }
+}
