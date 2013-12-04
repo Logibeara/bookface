953cc84d300544f49e53eac10edb845b6f4a2ecf
unknown <jamese@iastate.edu>
2013-12-04 17:00:33 -0600

    fixed the models for listing and have most of the bokshelf controler implimented

--- MvcApplication1/MvcApplication1/Controllers/BookshelfController.cs
+++ MvcApplication1/MvcApplication1/Controllers/BookshelfController.cs
@@ -45,16 +45,45 @@ namespace MvcApplication1.Controllers
             if (user != null)
                 {
                     //getUserID from logans utility
-                    Listing listToAdd = new Listing();
-
+                    using (TestDbContext db = new TestDbContext())
+                    {
+                        Book bookToCheck = new Book();
+                        //TODO set the fields of book accoring to the data in the dialog
+                        Listing listToAdd = new Listing();
+                        //adduser to listing
+                        //see if book exists in database
+                        if(db.Books.Contains(bookToCheck))
+                        {   
+                            Book bookChecked = db.Books.Where(b => b.BookName.Equals(bookToCheck.BookName) &&
+                       b.Course.CourseName.Equals(bookToCheck.Course) &&
+                       (bookToCheck.CourseID == b.Course.CourseNumber) &&
+                       b.Author.Equals(bookToCheck.Author)).First();
+                            if (bookChecked!= null)
+                            {
+                                listToAdd.BookID = bookChecked.BookID;
+                            }
+                            else
+                            {
+                                db.Books.Add(bookToCheck);
+                            }
+                            //ifnot add the book to the database
+                            //add the bookID to the Listing
+                        }
+                        //add the Listing date to the Listing object
+                        listToAdd.ListDate = DateTime.Now;
+                        //set list type to 0
+                        listToAdd.ListType = 0;
+                        //add the liting to the database
+                        db.Listings.Add(listToAdd);
+                    }
                 }
-            return null;
+            return Content("Book Added");
         }
 
         [HttpGet]
         public ActionResult AddBookDialog()
         {
-            return View("~/Views/Bookshelf/AddBook.cshtml");
+            return View("~/Views/Bookshelf/AddBook.cshtml"); 
         }
 
         //book details popup getter
