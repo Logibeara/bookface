c1db340c23f774dab9a87090f9d512dc8c752ffd
unknown <jamese@iastate.edu>
2013-12-05 01:05:00 -0600

    I think this should wokr but I need to pull some htings to see if it will, but I cave things that need to be merged... so yeah...

--- MvcApplication1/MvcApplication1/Controllers/BookshelfController.cs
+++ MvcApplication1/MvcApplication1/Controllers/BookshelfController.cs
@@ -39,51 +39,52 @@ namespace MvcApplication1.Controllers
         }
 
         [HttpPost]
-        public ActionResult AddBook(Listing toAdd, int userID)
+        public ActionResult AddBook(String bookName = "none", String iSBN = "none", String author = "none", String price = "$0.00")
         {
             String user = User.Identity.Name;
             if (user != null)
+            {
+                //getUserID from logans utility
+                using (TestDbContext db = new TestDbContext())
                 {
-                    //getUserID from logans utility
-                    using (TestDbContext db = new TestDbContext())
+                    Book bookToCheck = new Book();
+                    bookToCheck.BookName = bookName;
+                    bookToCheck.ISBN = iSBN;
+                    bookToCheck.Author = author;
+                    //TODO set the fields of book accoring to the data in the dialog
+                    Listing listToAdd = new Listing();
+                    //adduser to listing
+                    //see if book exists in database
+                    Book bookChecked = db.Books.Where(b => b.BookName.Equals(bookToCheck.BookName) &&
+               b.Author.Equals(bookToCheck.Author)).First();
+                    if (bookChecked != null)
                     {
-                        Book bookToCheck = new Book();
-                        //TODO set the fields of book accoring to the data in the dialog
-                        Listing listToAdd = new Listing();
-                        //adduser to listing
-                        //see if book exists in database
-                        if(db.Books.Contains(bookToCheck))
-                        {   
-                            Book bookChecked = db.Books.Where(b => b.BookName.Equals(bookToCheck.BookName) &&
-                       b.Course.CourseName.Equals(bookToCheck.Course) &&
-                       (bookToCheck.CourseID == b.Course.CourseNumber) &&
-                       b.Author.Equals(bookToCheck.Author)).First();
-                            if (bookChecked!= null)
-                            {
-                                listToAdd.BookID = bookChecked.BookID;
-                            }
-                            else
-                            {
-                                db.Books.Add(bookToCheck);
-                            }
-                            //ifnot add the book to the database
-                            //add the bookID to the Listing
-                        }
-                        //add the Listing date to the Listing object
-                        listToAdd.ListDate = DateTime.Now;
-                        //set list type to 0
-                        listToAdd.ListType = 0;
-                        //add the liting to the database
-                        db.Listings.Add(listToAdd);
+                        listToAdd.BookID = bookChecked.BookID;
                     }
+                    else
+                    {
+                        db.Books.Add(bookToCheck);
+                        bookChecked = db.Books.Where(b => b.BookName.Equals(bookToCheck.BookName) &&
+               b.Author.Equals(bookToCheck.Author)).First();
+                        listToAdd.BookID = bookChecked.BookID;
+                    }
+                    //ifnot add the book to the database
+                    //add the bookID to the Listing
+                    //add the Listing date to the Listing object
+                    listToAdd.ListDate = DateTime.Now;
+                    //set list type to 0
+                    listToAdd.ListType = 0;
+                    //add the liting to the database
+                    db.Listings.Add(listToAdd);
                 }
+            }
             return Content("Book Added");
         }
 
         [HttpGet]
         public ActionResult AddBookDialog()
         {
-            return View("~/Views/Bookshelf/AddBook.cshtml"); 
+            return View("~/Views/Bookshelf/AddBook.cshtml");
         }
 
         //book details popup getter
